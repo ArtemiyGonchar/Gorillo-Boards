8817cd70ac2eaba7640d7f06c16fdf840a120fc3
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-06-15 16:21:17 +0300

    Initialized DAL-Initializator for creating db when DAL package is triggered

---
+++ API/DAL-Nugget/DAL-Initializator/DAL-Initializator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DAL_Initializator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
