9f8b4c0414e1a912be00dce2a1e5b5ebaacc5f47
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-06-15 17:00:29 +0300

    Added initializator for db, it initialize db with tables from DAL-Core entities

--- API/DAL-Nugget/DAL-Initializator/DAL-Initializator/Program.cs
+++ API/DAL-Nugget/DAL-Initializator/DAL-Initializator/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace DAL_Initializator
+﻿
+using DAL_Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DAL_Initializator
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<GorilloBoardsDbContext>();
+                context.Database.EnsureCreated();
+            }
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddUserSecrets<Program>();
+            }).ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<GorilloBoardsDbContext>(options =>
+                    options.UseSqlServer(context.Configuration.GetConnectionString("DbConnectionString")));
+            });
     }
 }
