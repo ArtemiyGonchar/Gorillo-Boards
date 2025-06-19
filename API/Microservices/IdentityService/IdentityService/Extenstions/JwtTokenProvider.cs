65728fab912ce20833dcb69a0bdc0541b0ceb47c
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-06-19 18:22:57 +0300

    Added JwtTokenProvider and ServiceCollectionExtensions. JwtTokenProvider stands for creating new jwt tokens for users. ServiceCollectionExtensions stands for injecting services to middleware. For now it injects JwtTokenProvider and adding authorization for swagger

---
+++ API/Microservices/IdentityService/IdentityService/Extenstions/JwtTokenProvider.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLogicLayer.DTO;
+using DataAccessLayer.Entities;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+namespace PresentationLayer.Extenstions
+{
+    public class JwtTokenProvider
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string CreateToken(UserJwtDTO user)
+        {
+            string secretKey = _configuration["Jwt:Secret"]; //in user secrets
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)); //TODO add null check
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                    [
+                        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                        new Claim(ClaimTypes.Name, user.UserName),
+                        new Claim(ClaimTypes.Role, user.Role.ToString())
+                    ]),
+                Expires = DateTime.UtcNow.AddMinutes(_configuration.GetValue<int>("Jwt:ExpirationInMinutes")),
+                SigningCredentials = credentials,
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"]
+            };
+
+            var handler = new JsonWebTokenHandler();
+
+            string token = handler.CreateToken(tokenDescriptor);
+
+            return token;
+        }
+    }
+}
---
+++ API/Microservices/IdentityService/IdentityService/Extenstions/ServiceCollectionExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace PresentationLayer.Extenstions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSwaggerGenWithJWT(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(o =>
+            {
+                //o.SwaggerDoc("v1", new() { })
+
+                o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer"
+                });
+
+                o.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer",
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+            return services;
+        }
+
+        public static IServiceCollection AddJwtTokenProvider(this IServiceCollection services)
+        {
+            services.AddScoped<JwtTokenProvider>();
+            return services;
+        }
+    }
+}
