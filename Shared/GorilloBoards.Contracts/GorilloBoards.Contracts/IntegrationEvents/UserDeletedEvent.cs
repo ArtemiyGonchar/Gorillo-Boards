5cef7612339365bbb632b4dcaf4e0b84416a0487
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-06-20 20:32:55 +0300

    Updated Contracts. Added contracts for user deleting and updating

---
+++ Shared/GorilloBoards.Contracts/GorilloBoards.Contracts/IntegrationEvents/UserDeletedEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GorilloBoards.Contracts.IntegrationEvents
+{
+    public class UserDeletedEvent
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+    }
+}
