a515ec1e63947770b687731589186898d061aabe
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-06-22 15:02:40 +0300

    Adde GetBoardByTitle to BoardRepository in DAL of BoardsService

--- API/Microservices/BoardsService/DataAccessLayer/Repositories/BoardRepository.cs
+++ API/Microservices/BoardsService/DataAccessLayer/Repositories/BoardRepository.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccessLayer.DatabaseContext;
 using DataAccessLayer.Entities;
 using DataAccessLayer.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,11 @@ namespace DataAccessLayer.Repositories
         public BoardRepository(BoardsDbContext ctx) : base(ctx)
         {
             _ctx = ctx;
+        }
+
+        public async Task<Board?> GetBoardByTitle(string title)
+        {
+            return await _ctx.Set<Board>().SingleOrDefaultAsync(b => b.Title == title);
         }
     }
 }
--- API/Microservices/BoardsService/DataAccessLayer/Repositories/Interfaces/IBoardRepository.cs
+++ API/Microservices/BoardsService/DataAccessLayer/Repositories/Interfaces/IBoardRepository.cs
@@ -9,5 +9,6 @@ namespace DataAccessLayer.Repositories.Interfaces
 {
     public interface IBoardRepository : IRepository<Board>
     {
+        Task<Board?> GetBoardByTitle(string title);
     }
 }
