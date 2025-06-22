756ce7d8f184b7d6d280a89abbdf7993d1efc72e
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-06-22 14:59:14 +0300

    Added Presentation Layer of BoardsService with jwt token and BoardsManagmentController

---
+++ API/Microservices/BoardsService/BoardsService/Controllers/BoardsManagmentController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLogicLayer.DTO;
+using BusinessLogicLayer.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoardsService.Controllers
+{
+    [Route("api/admin/boardmanagment")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class BoardsManagmentController : Controller
+    {
+        private readonly IBoardsManagmentService _boardsManagmentService;
+
+        public BoardsManagmentController(IBoardsManagmentService boardsManagmentService)
+        {
+            _boardsManagmentService = boardsManagmentService;
+        }
+
+        [HttpPost("CreateBoard")]
+        public async Task<IActionResult> CreateBoard([FromBody] BoardCreateDTO boardCreateDTO)
+        {
+            var boardId = await _boardsManagmentService.CreateBoardAsync(boardCreateDTO);
+            return Ok(boardId);
+        }
+    }
+}
