854f4230e21dc20e9521dbb0d632116684cf4587
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-07-14 15:21:17 +0300

    Add: TicketClosedEvent, TicketCreatedEvent, TicketDeletedEvent AT: GorilloBoards.Contracts

---
+++ Shared/GorilloBoards.Contracts/GorilloBoards.Contracts/IntegrationEvents/TicketCreatedEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GorilloBoards.Contracts.IntegrationEvents
+{
+    public class TicketCreatedEvent
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
